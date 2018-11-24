0b079e20de724ddf1d513170d402b57e80f38be4
MemoryTing <44110712+MemoryTing@users.noreply.github.com>
2018-11-24 16:38:52 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<fstream>
+#include<vector>
 
 using namespace std;
 
@@ -17,8 +18,54 @@ using namespace std;
      fin.open(in_com);
      fout.open(out_com);
 	 
-	 
-	 
+	 int B_x , B_y;
+	 int r , c , B;
+	 int direction=0;
+	 int need_clear=0;
+	 vector<int> step_x;
+	 vector<int> step_y;
+	 fin >> r >> c >> B;
+	 char map[r][c];
+	 for (int i=0 ; i<=r+1 ; i++){
+		for (int j=0 ; j<=c+1 ; j++){
+			if(i==0 || j == 0 || i==r+1 || j == c+1) map[i][j] = 3;
+			else {
+				fin >> map[i][j];
+				if(map[i][j] == 'R'){
+					B_x = i;
+					B_y = j;
+				}
+				else if(map[i][j] == '0'){
+					need_clear++;
+				}
+			}
+		}
+	 }
+	 step_x.clear();
+	 step_y.clear();
+	 int now_x = B_x;
+	 int now_y = B_y;
+	 while(need_clear>0){
+		if(now_x == B_x && now_y == B_y){
+			if()
+		if(direction == 0){  //Up
+			if(map[now_x-1][now_y] == '0'){
+				if(map[now])
+				now_x = now_x-1;
+				map[now_x][now_y] = 2;
+				need_clear--;
+			}
+			else if(map[now_x-1][now_y] == '1'|| map[now_x-1][now_y] == '2'){
+				direction = 1;
+		}
+		else if(direction == 1){ //Left
+			
+		}
+		else if(direction == 2){ //Right
+		}
+		else { //Down
+		}
+	 }
 	 fin.close();
      fout.close();
      return 0;
