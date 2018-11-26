f5cecfc224c0cd39c63f613f2f90d698f5321105
MemoryTing <44110712+MemoryTing@users.noreply.github.com>
2018-11-26 11:39:02 +0800

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -4,9 +4,12 @@
 
 using namespace std;
 
-int B_x , B_y , r , c , step;
+int B_x , B_y , r , c , step , b;
 vector<int> step_x;
 vector<int> step_y;
+bool wall_at_right;
+bool no_wall;
+int need_clear;
 void back_to_R(int x , int y)
 {
 	int n=step_x.size()-1;
@@ -24,6 +27,100 @@ void back_to_R(int x , int y)
 	return;
 }	
 
+void go_up(int now_x , int now_y)
+{
+	if(map[now_x-1][now_y] == '0'){
+		now_x = now_x-1;
+		map[now_x][now_y] = '2';
+		step_x.push_back(now_x);
+		step_y.push_back(now_y);
+		step++;
+		b--;
+		need_clear--;
+		if(wall_at_right) {
+			if(map[now_x][now_y+1] == '0') go_right(now_x , now_y);
+		}
+		else if(!no_wall){
+			if(map[now_x][now_y-1] == '0') go_left(now_x , now_y);
+		}
+		else go_up(now_x , now_y);
+	}
+	else {
+		if(wall_at_right) go_left(now_x , now_y);
+		else if (no_wall){
+			no_wall = false;
+			wall_at_right = true;
+			go_left(now_x , now_y);
+		}
+		else go_right(now_x , now_y);
+	}
+}
+void go_left(int now_x , int now_y)
+{
+	if(map[now_x][now_y-1] == '0'){
+		now_y = now_y-1;
+		map[now_x][now_y] = '2';
+		step_x.push_back(now_x);
+		step_y.push_back(now_y);
+		step++;
+		b--;
+		need_clear--;
+		if(wall_at_right) {
+			if(map[now_x-1][now_y] == '0') go_up(now_x , now_y);
+		}
+		else {
+			if(map[now_x+1][now_y] == '0') go_down(now_x , now_y);
+		}
+	}
+	else {
+		if(wall_at_right) go_down(now_x , now_y);
+		else go_up(now_x , now_y);
+	}
+}
+void go_right(int now_x , int now_y)
+{
+	if(map[now_x][now_y+1] == '0'){
+		now_y = now_y+1;
+		map[now_x][now_y] = '2';
+		step_x.push_back(now_x);
+		step_y.push_back(now_y);
+		step++;
+		b--;
+		need_clear--;
+		if(wall_at_right) {
+			if(map[now_x+1][now_y] == '0') go_down(now_x , now_y);
+		}
+		else {
+			if(map[now_x-1][now_y] == '0') go_up(now_x , now_y);
+		}
+	}
+	else {
+		if(wall_at_right) go_up(now_x , now_y);
+		else go_down(now_x , now_y);
+	}
+}
+void go_down(int now_x , int now_y)
+{
+	if(map[now_x+1][now_y] == '0'){
+		now_x = now_x+1;
+		map[now_x][now_y] = '2';
+		step_x.push_back(now_x);
+		step_y.push_back(now_y);
+		step++;
+		b--;
+		need_clear--;
+		if(wall_at_right) {
+			if(map[now_x][now_y-1] == '0') go_left(now_x , now_y);
+		}
+		else {
+			if(map[now_x][now_y+1] == '0') go_right(now_x , now_y);
+		}
+	}
+	else {
+		if(wall_at_right) go_right(now_x , now_y);
+		else go_left(now_x , now_y);
+	}
+}
  int main(int argc , char *argv[])
  {
      ifstream fin;
@@ -38,10 +135,7 @@ void back_to_R(int x , int y)
      fin.open(in_com);
      fout.open(out_com);
 	 
-	 int b;
 	 int B;
-	 int direction=0;
-	 int need_clear=0;
 	 fin >> r >> c >> B;
 	 char map[r][c];
 	 for (int i=0 ; i<=r+1 ; i++){
@@ -63,11 +157,9 @@ void back_to_R(int x , int y)
 	 step_y.clear();
 	 int now_x = B_x;
 	 int now_y = B_y;
-	 bool wall_at_right = false;
-	 bool no_wall = false;
 	 b = B;
 	 step = 0;
-	 //while(need_clear>0){
+	 while(need_clear>0){
 		if(b < B/2+1){
 			back_to_R(now_x , now_y);
 			now_x = B_x;
@@ -76,15 +168,6 @@ void back_to_R(int x , int y)
 		}
 		if(now_x == B_x && now_y == B_y){
 			if(map[now_x-1][now_y] == '0'){
-				if(map[now_x][now_y+1] == '1') {
-					direction = 2;
-					wall_at_right = true;
-				}
-				else if(map[now_x][now_y-1] == '1') direction = 1;
-				else {
-					direction = 0;
-					no_wall = true;
-				}
 				now_x = now_x-1;
 				map[now_x][now_y] = '2';
 				if(step == 0) B_x = B_x-1;
@@ -93,9 +176,20 @@ void back_to_R(int x , int y)
 				step++;
 				b--;
 				need_clear--;
+				if(map[now_x+1][now_y+1] == '1') {
+					wall_at_right = true;
+					go_right(now_x , now_y);
+				}
+				else if(map[now_x+1][now_y-1] == '1') {
+					wall_at_right = false;
+					go_left(now_x , now_y);
+				}
+				else {
+					no_wall = true;
+					go_up(now_x , now_y);
+				}
 			}
 			else if(map[now_x][now_y-1] == '0'){
-				direction = 0;
 				wall_at_right = true;
 				now_y = now_y-1;
 				map[now_x][now_y] = '2';
@@ -130,95 +224,7 @@ void back_to_R(int x , int y)
 				need_clear--;
 			}
 		}
-		if(direction == 0){  //Up
-			if(map[now_x-1][now_y] == '0'){
-				now_x = now_x-1;
-				map[now_x][now_y] = '2';
-				step_x.push_back(now_x);
-				step_y.push_back(now_y);
-				step++;
-				b--;
-				need_clear--;
-				if(wall_at_right) {
-					if(map[now_x][now_y+1] == '0') direction = 2;
-				}
-				else if(!no_wall){
-					if(map[now_x][now_y-1] == '0') direction = 1;
-				}
-			}
-			else {
-				if(wall_at_right) direction = 1;
-				else if (no_wall){
-					direction = 1;
-					no_wall = false;
-					wall_at_right = true;
-				}
-				else direction = 2;
-			}
-		}
-		else if(direction == 1){ //Left
-			if(map[now_x][now_y-1] == '0'){
-				now_y = now_y-1;
-				map[now_x][now_y] = '2';
-				step_x.push_back(now_x);
-				step_y.push_back(now_y);
-				step++;
-				b--;
-				need_clear--;
-				if(wall_at_right) {
-					if(map[now_x-1][now_y] == '0') direction = 1;
-				}
-				else {
-					if(map[now_x+1][now_y] == '0') direction = 3;
-				}
-			}
-			else {
-				if(wall_at_right) direction = 3;
-				else direction = 0;
-			}
-		}
-		else if(direction == 2){ //Right
-			if(map[now_x][now_y+1] == '0'){
-				now_y = now_y+1;
-				map[now_x][now_y] = '2';
-				step_x.push_back(now_x);
-				step_y.push_back(now_y);
-				step++;
-				b--;
-				need_clear--;
-				if(wall_at_right) {
-					if(map[now_x+1][now_y] == '0') direction = 3;
-				}
-				else {
-					if(map[now_x-1][now_y] == '0') direction = 1;
-				}
-			}
-			else {
-				if(wall_at_right) direction = 0;
-				else direction = 3;
-			}
-		}
-		else { //Down
-			if(map[now_x+1][now_y] == '0'){
-				now_x = now_x+1;
-				map[now_x][now_y] = '2';
-				step_x.push_back(now_x);
-				step_y.push_back(now_y);
-				step++;
-				b--;
-				need_clear--;
-				if(wall_at_right) {
-					if(map[now_x][now_y-1] == '0') direction = 1;
-				}
-				else {
-					if(map[now_x][now_y+1] == '0') direction = 2;
-				}
-			}
-			else {
-				if(wall_at_right) direction = 2;
-				else direction = 1;
-			}
-		}
+	 }
 	 fout << step << endl;
 	 for(int i=0 ; i<step ; i++){
 		 fout << step_x[i] << ' ';
